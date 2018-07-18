3ad711e30e3f5d2d54cdf165f82924d698ee54c0
fddata <fraserjdouglas@gmail.com>
2018-07-18 17:48:59 +0100

    exercise b complete

---
+++ exercise_b.rb
@@ -0,0 +1,104 @@
+# Exercise B
+#
+# Given the following data structure:
+
+users = {
+  "Jonathan" => {
+    :twitter => "jonnyt",
+    :lottery_numbers => [6, 12, 49, 33, 45, 20],
+    :home_town => "Stirling",
+    :pets => [
+    {
+      :name => "fluffy",
+      :species => "cat"
+    },
+    {
+      :name => "fido",
+      :species => "dog"
+    },
+    {
+      :name => "spike",
+      :species => "dog"
+    }
+  ]
+  },
+  "Erik" => {
+    :twitter => "eriksf",
+    :lottery_numbers => [18, 34, 8, 11, 24],
+    :home_town => "Linlithgow",
+    :pets => [
+    {
+      :name => "nemo",
+      :species => "fish"
+    },
+    {
+      :name => "kevin",
+      :species => "fish"
+    },
+    {
+      :name => "spike",
+      :species => "dog"
+    },
+    {
+      :name => "rupert",
+      :species => "parrot"
+    }
+  ]
+  },
+  "Avril" => {
+    :twitter => "bridgpally",
+    :lottery_numbers => [12, 14, 33, 38, 9, 25],
+    :home_town => "Dunbar",
+    :pets => [
+      {
+        :name => "monty",
+        :species => "snake"
+      }
+    ]
+  }
+}
+# Complete these tasks:
+#
+# Get Jonathan's Twitter handle (i.e. the string "jonnyt")
+p users['Jonathan'][:twitter]
+
+# Get Erik's hometown
+p users['Erik'][:home_town]
+
+# Get the array of Erik's lottery numbers
+p users['Erik'][:lottery_numbers]
+
+# Get the type of Avril's pet Monty
+ p users['Avril'][:pets][0][:species]
+
+# Get the smallest of Erik's lottery numbers
+p "The smallest lottery number is " + users['Erik'][:lottery_numbers].min().to_s()
+
+# Return an array of Avril's lottery numbers that are even
+p users['Avril'][:lottery_numbers].select{|num| num.even?}
+
+
+# Erik is one lottery number short! Add the number 7 to be included in his lottery numbers
+p users['Erik'][:lottery_numbers].push(7)
+
+# Change Erik's hometown to Edinburgh
+users['Erik'][:home_town] = "Edinburgh"
+
+# Add a pet dog to Erik called "Fluffy"
+users['Erik'][:pets] = {
+  name: 'Fluffy',
+  species: 'dog',
+}
+
+# Add another person to the users hash
+fraser_hash = { "Fraser" => {
+  :twitter => "not on it!",
+  :lottery_numbers => [1, 2, 3, 4, 45, 20, 9],
+  :home_town => "Ayr",
+  :pets => [  {    :name => "milly",    :species => "dog"  },  ]
+  }
+}
+
+#updating in place using merge!
+users.merge!(fraser_hash)
+p users
